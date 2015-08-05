c0769a257d49194738857a510853f577023ad059
Artur Teregulov <sliceice@gmail.com>
2015-08-05 18:30:20 +0300

    Target BuildApp

--- build.fsx
+++ build.fsx
@@ -2,10 +2,27 @@
 #r @"packages\FAKE.4.0.3\tools\FakeLib.dll"
 open Fake
 
-// Default target
+// Properties
+let buildDir = "./build/Default"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+Target "BuildApp" (fun _ ->
+    !! @"D:\Rep\Promo.EverythingIsNew\Promo.EverythingIsNew.WebApp/Promo.EverythingIsNew.WebApp.csproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+  ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
