0121d286fb8176b8ad4a2ef43e9b7de50ac9b404
Artur Teregulov <sliceice@gmail.com>
2015-08-28 17:46:58 +0300

    sample for EnterpriseLibrary.SemanticLogging.Elasticsearch

--- Promo.EverythingIsNew.WebApp/Global.asax.cs
+++ Promo.EverythingIsNew.WebApp/Global.asax.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using FullScale180.SemanticLogging;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using Promo.EverythingIsNew.WebApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +20,17 @@ namespace Promo.EverythingIsNew.WebApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            var listener = new ObservableEventListener();
+            listener.EnableEvents(TestEvents.Log, EventLevel.LogAlways, Keywords.All);
+
+            listener.LogToConsole();
+            listener.LogToElasticsearch("SLABEL", "http://localhost:9200", "myindex", "mytype", bufferingCount: 1);
+
+            TestEvents.Log.Critical("Hello world In-Process Critical");
+            TestEvents.Log.Error("Hello world In-Process Error");
+            TestEvents.Log.Informational("Hello world In-Process Informational");
         }
     }
 }
