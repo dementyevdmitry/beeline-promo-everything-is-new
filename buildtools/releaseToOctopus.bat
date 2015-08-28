01e5f818f2f3127e1052977a538e8b733ed3b5f6
Artur Teregulov <sliceice@gmail.com>
2015-08-28 12:02:25 +0300

    update commontargets

--- buildtools/releaseToOctopus.bat
+++ buildtools/releaseToOctopus.bat
@@ -1,5 +1,5 @@
 pushd %~dp0
 pushd ..
-powershell buildtools\build.ps1 -MainVersion 1.2.0 -BuildNumber 21 -BuildScript "buildtools\releaseToOctopus.fsx"
+powershell buildtools\build.ps1 -MainVersion 1.2.0 -BuildNumber 22 -BuildScript "buildtools\releaseToOctopus.fsx"
 popd
 popd
