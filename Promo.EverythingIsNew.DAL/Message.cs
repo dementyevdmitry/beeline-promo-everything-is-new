9417ea9a513c341c54ef731432f5e13d1f70864d
Artur Teregulov <sliceice@gmail.com>
2015-10-05 15:52:19 +0300

    API mapping classes updated

---
+++ Promo.EverythingIsNew.DAL/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promo.EverythingIsNew.DAL
+{
+    public class Message
+    {
+        public string ctn { get; set; } // String(10)	Поле обязательно, Номер абонента в сети Билайн
+        public string uid { get; set; } // String(20)	Поле обязательно, Идентификатор пользователя в системе, связанной с LP
+        public string email { get; set; } // String(129) не обязательно, Email адрес абонента
+    }
+}
--- Promo.EverythingIsNew.DAL/Status.cs
+++ Promo.EverythingIsNew.DAL/Status.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace Promo.EverythingIsNew.DAL
 {
-    public class ClientStatusGetDto
+    public class Status
     {
         public string ctn { get; set; } // String(10)	Обязательно:Да	Номер абонента в сети Билайн
-        public string uid { get; set; } // String(10)	Идентификатор пользователя в системе, связанной с LP
-        public string email { get; set; } // String(129)
+        public string uid { get; set; } // String(20)	Идентификатор пользователя в системе, связанной с LP
     }
 }
