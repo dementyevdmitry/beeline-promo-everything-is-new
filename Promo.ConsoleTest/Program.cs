342f611925a04529a01acf203f52f1a15fe49c77
Artur Teregulov <sliceice@gmail.com>
2015-10-05 17:42:42 +0300

    ConsoleTest project for remoting integration testing

---
+++ Promo.ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Promo.ConsoleTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //var client = new HttpClient();
+
+            //Console.WriteLine(ConfigurationManager.AppSettings["Key"].ToString());
+
+        }
+    }
+}
