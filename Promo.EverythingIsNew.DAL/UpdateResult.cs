dac8a8e57adb9462cac938ed1929e809cdbbd017
Artur Teregulov <sliceice@gmail.com>
2015-10-06 16:39:29 +0300

    fix UpdateResult

--- Promo.EverythingIsNew.DAL/UpdateResult.cs
+++ Promo.EverythingIsNew.DAL/UpdateResult.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Promo.EverythingIsNew.DAL
 {
-    class UpdateResult
-
+    public class UpdateResult
     {
         public bool status { get; set; }  // Boolean поле обязательно, Статус получения персональных данных. Если статус True. Тогда операция прошла успешно и ошибок не было обнаружено.
         public string description { get; set; }  // String(500)	Причина ошибки. Не пустое, если Status false. 
