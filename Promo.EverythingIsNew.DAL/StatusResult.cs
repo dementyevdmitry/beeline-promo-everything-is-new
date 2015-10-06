8d685868aa5d6288bc6f28150ed140b1ba6a2592
Artur Teregulov <sliceice@gmail.com>
2015-10-06 16:52:37 +0300

    fix StatusResult
    (cherry picked from commit 3eed3012e633e880ab2baa6fe4a2a8fdb670eb2d)

--- Promo.EverythingIsNew.DAL/StatusResult.cs
+++ Promo.EverythingIsNew.DAL/StatusResult.cs
@@ -11,6 +11,7 @@ namespace Promo.EverythingIsNew.DAL
         public bool is_beeline_subscriber { get; set; } // принадлежит ли номер абоненту билайн
         public bool is_used_uid { get; set; } // участвовал ли данный аккаунт в акции
         public bool is_used_ctn { get; set; } // участвовал ли данный абонент в акции
+        public bool Is_used_uid_with_ctn { get; set; } //Присутствует если UID и CTN уже использовались. Если в одной связке, то вернется true, если связка в прошлый раз была другой, то false
 
     }
 }
