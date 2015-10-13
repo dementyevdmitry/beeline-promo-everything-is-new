9b1e395ed5cfa2250ed75fd473d52d669f280dba
Artur Teregulov <sliceice@gmail.com>
2015-10-13 17:45:38 +0300

    new routes

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("AltLanDS")]
 [assembly: AssemblyCopyrightAttribute("Copyright © AltLanDS 2012-2015")]
 [assembly: AssemblyVersionAttribute("1.2.0")]
-[assembly: AssemblyFileVersionAttribute("1.2.0.22")]
+[assembly: AssemblyFileVersionAttribute("1.2.0.23")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.2.0";
--- Promo.EverythingIsNew.WebApp/App_Start/RouteConfig.cs
+++ Promo.EverythingIsNew.WebApp/App_Start/RouteConfig.cs
@@ -14,9 +14,27 @@ namespace Promo.EverythingIsNew.WebApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "choose",
+                url: "choose",
+                defaults: new { controller = "home", action = "choose"}
+            );
+
+            routes.MapRoute(
+                name: "index",
+                url: "index",
+                defaults: new { controller = "home", action = "index" }
+            );
+
+            routes.MapRoute(
+                name: "offer",
+                url: "offer",
+                defaults: new { controller = "home", action = "offer" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "choose", id = UrlParameter.Optional }
             );
         }
     }
