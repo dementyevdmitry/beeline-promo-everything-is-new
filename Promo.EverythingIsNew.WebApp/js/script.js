2f937bc3dd373ca56876a9846715b21e4442a884
Artur Teregulov <sliceice@gmail.com>
2015-10-21 15:14:32 +0300

    submit button

--- Promo.EverythingIsNew.WebApp/js/script.js
+++ Promo.EverythingIsNew.WebApp/js/script.js
@@ -1,48 +1,54 @@
-$(document).on('click', ".checkbox", function(e) {
-	if($(e.target).closest("a").length) return true;
-	
-	var $hidden = $(this).find("input");
-	var $cont = $(this);//.closest(".checkbox");
+$(function () {
+    $(document).on('click', ".checkbox", function (e) {
+        if ($(e.target).closest("a").length) return true;
 
-	if($hidden.is(":checked")) {
-		$cont.removeClass("checked");
-		$hidden.prop('checked', false);
-	} else {
-		$cont.addClass("checked");
-		$hidden.prop('checked', true);
-	}
-	
-	return false;
-});
+        var $hidden = $(this).find("input");
+        var $cont = $(this);//.closest(".checkbox");
 
-$(document).on('click', ".fold-link", function(e) {
-	var $link = $(this),
-		$foldable = $link.next(".foldable");
-	
-	$foldable.slideToggle(300);
-	$link.toggleClass("open");
+        if ($hidden.is(":checked")) {
+            $cont.removeClass("checked");
+            $hidden.prop('checked', false);
+        } else {
+            $cont.addClass("checked");
+            $hidden.prop('checked', true);
+        }
 
-	return false;
-});
+        return false;
+    });
 
-$(document).on("click", ".offer-choice .btn-link", function(e) {
-	var $choice = $(this).closest(".offer-choice"),
-		$success = $(".success-block");
-	
-	$choice.hide();
-	$success.show();
-	setTimeout(function() {
-		$success.addClass("show");
-	},100);
-});
+    $(document).on('click', ".fold-link", function (e) {
+        var $link = $(this),
+            $foldable = $link.next(".foldable");
 
-$(document).on('click', ".success-block", function(e) {
-	var T = $(this),
-		$circle = T.find(".circle");
-	
-	setTimeout(function() {
-		$circle.addClass("show");
-	},500);
+        $foldable.slideToggle(300);
+        $link.toggleClass("open");
 
-	return false;
+        return false;
+    });
+
+    $(document).on("click", ".offer-choice .btn-link", function (e) {
+        var $choice = $(this).closest(".offer-choice"),
+            $success = $(".success-block");
+
+        $choice.hide();
+        $success.show();
+        setTimeout(function () {
+            $success.addClass("show");
+        }, 100);
+    });
+
+    $(document).on('click', ".success-block", function (e) {
+        var T = $(this),
+            $circle = T.find(".circle");
+
+        setTimeout(function () {
+            $circle.addClass("show");
+        }, 500);
+
+        return false;
+    });
+
+    $("#submit-index").click(function () {
+        $('#form-index').submit();
+    });
 });
