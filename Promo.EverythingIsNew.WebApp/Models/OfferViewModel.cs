c9909a04fac545925a379f0fa3236a93513db57e
Artur Teregulov <sliceice@gmail.com>
2015-10-23 17:36:42 +0300

    add OfferViewModel

---
+++ Promo.EverythingIsNew.WebApp/Models/OfferViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Promo.EverythingIsNew.WebApp.Models
+{
+    public class OfferViewModel
+    {
+        public string UserName { get; set; }
+        public string TariffName { get; set; }
+
+        public string EverydayMinutesPackage { get; set; }
+        public string EverydayTrafficMbPackage { get; set; }
+        public string EverydaySmsPackage { get; set; }
+
+        public string EveryMontMinutesPackage { get; set; }
+        public string EveryMontMinutesRegion { get; set; }
+
+        public string EveryMontGbPackage { get; set; }
+        public string EveryMontGbRegion { get; set; }
+
+        public string EveryMontSmsPackage { get; set; }
+        public string EveryMontSmsRegion { get; set; }
+
+        public string SubscriptionFee { get; set; }
+        public string TransitionCost { get; set; }
+    }
+}
