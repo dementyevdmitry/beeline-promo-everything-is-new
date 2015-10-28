bccf60e8a9cf6c3a06ce65b6103efc9f777d8a20
Artur Teregulov <sliceice@gmail.com>
2015-10-28 15:58:27 +0300

    cookies encoding and viewmodels mapping moved to Helpers.cs

---
+++ Promo.EverythingIsNew.WebApp/Models/Helpers.cs
@@ -0,0 +1,90 @@
+﻿using AltLanDS.AllNew.Core;
+using Newtonsoft.Json;
+using Promo.EverythingIsNew.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Promo.EverythingIsNew.WebApp.Models
+{
+    public class Helpers
+    {
+        public static TariffGroupViewModel MapTariffGroup(int id, IEnumerable<AltLanDS.Beeline.DpcProxy.Client.Dpc.ProductParameter> lines)
+        {
+            return new TariffGroupViewModel
+            {
+                Id = id,
+                Name = lines.FirstOrDefault().Group.Title,
+                SortOrder = lines.FirstOrDefault().Group.SortOrder,
+                Lines = lines.Select(l => MapTariffLine(l)).OrderBy(s => s.SortOrder).ToList()
+            };
+        }
+
+        public static TariffLineViewModel MapTariffLine(AltLanDS.Beeline.DpcProxy.Client.Dpc.ProductParameter l)
+        {
+            return new TariffLineViewModel
+            {
+                Title = l.Title,
+                NumValue = l.NumValue.ToString(),
+                UnitDisplay = (l.Unit != null) ? l.Unit.Display : null,
+                Value = l.Value,
+                SortOrder = l.SortOrder
+            };
+        }
+
+        public static List<string> GetTestCities()
+        {
+            List<string> items = new List<string>();
+            items.Add("Москва");
+            items.Add("Санкт-Петербург");
+            items.Add("Петропавловск-Камчатский");
+            items.Add("Воронеж");
+            return items;
+        }
+
+        public static EntryForm MapToEntryForm(VkModel userData, AccessData accessData)
+        {
+            var model = userData.Response.Select(x =>
+                new EntryForm
+                {
+                    Academy = x.Academy,
+                    Birthday = x.Birthday,
+                    SelectMyCity = x.City.Title,
+                    Email = accessData.Email,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    CTN = x.Phone,
+                }).FirstOrDefault();
+            return model;
+        }
+
+        public static void EncodeToCookies(EntryForm userProfile, ControllerContext controllerContext)
+        {
+            var cookie = new HttpCookie("UserProfile");
+            var json = JsonConvert.SerializeObject(userProfile);
+            var bytes = Encoding.Unicode.GetBytes(json);
+            var encoded = MachineKey.Protect(bytes);
+            var base64 = Convert.ToBase64String(encoded);
+            cookie.Value = base64;
+            controllerContext.HttpContext.Response.Cookies.Add(cookie);
+        }
+
+        public static EntryForm DecodeFromCookies(ControllerContext controllerContext)
+        {
+            if (controllerContext.HttpContext.Request.Cookies.AllKeys.Contains("UserProfile"))
+            {
+                var cookie = controllerContext.HttpContext.Request.Cookies["UserProfile"];
+                var encoded = Convert.FromBase64String(cookie.Value);
+                var decoded = MachineKey.Unprotect(encoded);
+                var json = Encoding.Unicode.GetString(decoded);
+                EntryForm userProfile = JsonConvert.DeserializeObject<EntryForm>(json);
+                return userProfile;
+            }
+            return null;
+        }
+    }
+}
